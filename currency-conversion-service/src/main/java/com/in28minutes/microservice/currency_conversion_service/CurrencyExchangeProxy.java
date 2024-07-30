5df40c7e6dadb4df47b5666fe158f1d91445bd5d
Christine Peng <caipeng@Christines-Air.cable.rcn.com>
2024-07-29 22:06:07 -0400

    Initial commit

---
+++ currency-conversion-service/src/main/java/com/in28minutes/microservice/currency_conversion_service/CurrencyExchangeProxy.java
@@ -0,0 +1,15 @@
+package com.in28minutes.microservice.currency_conversion_service;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+//@FeignClient(name = "current-exchange", url="localhost:8000")
+@FeignClient(name="current-exchange")
+public interface CurrencyExchangeProxy {
+
+    @GetMapping("/currency-exchange/from/{from}/to/{to}")
+    public CurrencyConversion retrieveExchangeValue(
+            @PathVariable String from,
+            @PathVariable String to);
+}
